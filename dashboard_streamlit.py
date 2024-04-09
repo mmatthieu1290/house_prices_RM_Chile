fd3296a76ee45304d74a7ef4793899888d03a133
mmathieu1290 <mathieu1290@gmail.com>
2024-04-08 23:38:44 -0400

    actualizate

--- dashboard_streamlit.py
+++ dashboard_streamlit.py
@@ -39,9 +39,9 @@ dormitorios = st.sidebar.number_input("Indique el numero de habitaciones",1)
 
 banios = st.sidebar.number_input("Indique el numero de baños",1)
 
-mts_utiles = float(st.sidebar.number_input("Indique el metraje util",1))
+mts_utiles = float(st.sidebar.number_input("Indique el metraje util",100))
 
-mts_total = float(st.sidebar.number_input("Indique el metraje total",1))
+mts_total = float(st.sidebar.number_input("Indique el metraje total",100))
 
 parkings = float(st.sidebar.number_input("Indique el numero de estacionamientos",1))
 
