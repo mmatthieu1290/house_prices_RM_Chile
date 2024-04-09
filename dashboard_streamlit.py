e4589a527a1363e7916f4a36b133d157bb0bbd7f
mmathieu1290 <mathieu1290@gmail.com>
2024-04-08 23:57:56 -0400

    actualizate

--- dashboard_streamlit.py
+++ dashboard_streamlit.py
@@ -48,7 +48,7 @@ parkings = float(st.sidebar.number_input("Indique el numero de estacionamientos"
 
 X = np.array([[dormitorios,banios,mts_utiles,mts_total,parkings]])
 
-price = LR.predict(X)[0][0],
+price = mts_utiles * LR.predict(X)[0],
 
 st.write(price)
 
