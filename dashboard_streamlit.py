a9aeb930a8208137633bcfe10dafc72004e2a3dc
mmathieu1290 <mathieu1290@gmail.com>
2024-04-29 21:38:14 -0400

    actualisation

--- dashboard_streamlit.py
+++ dashboard_streamlit.py
@@ -47,7 +47,7 @@ mts_total = float(st.sidebar.number_input("Indique el metraje total",min_value =
 parkings = float(st.sidebar.number_input("Indique el numero de estacionamientos",1))
 
 
-X = np.array([[dormitorios,banios,mts_utiles,mts_total,parkings]])
+X = np.array([[dormitorios,banios,mts_utiles,parkings]])
 
 price = mts_utiles * LR.predict(X)[0],
 
